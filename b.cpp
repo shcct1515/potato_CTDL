bf2e447a69841c31d92b8a8ba11f5e316e05eca2
shcct <shcct1254@gmail.com>
2025-02-20 21:55:34 +0700

    update b.py

--- b.cpp
+++ b.cpp
@@ -1,8 +1,20 @@
 #include<stdio.h>
 
-int main(){
-    int n;
-    int a[n];
+double power(float a, int n){
     
+    if (n==0){
+        return 1;
+    }
+    if (n%2==0){
+        return (power(a,n/2)*power(a,n/2));
+    }
+    else{
+        return (power(a, n/2) * power(a, n/2) * a);
+        
+    }
+}
+
+int main(){
+    printf("%.4f\n", power(2, 3));
     return 0;
 }
