af05970e6a04ae84258f6f933d9384d7521a74a9
shcct <shcct1254@gmail.com>
2025-02-21 19:23:26 +0700

    update b.cpp

--- b.cpp
+++ b.cpp
@@ -2,30 +2,40 @@
 #include<stdlib.h>
 
 typedef int elementtype;
-typedef struct
+typedef struct node
 {
     elementtype element;
-    node* next;
+    struct node* next;
 }node;
 typedef node* list;
 typedef list position;
 
 list makenull(){
     list head = (node*)malloc(sizeof(node));
-    head = NULL;
     head->next = NULL;
+    return head;
 }
 
-list insertafterhead(elementtype x, list l){
+void insertafterhead(elementtype x, list l){
     node* newNode = (node*)malloc(sizeof(node));
     newNode->element = x;
     newNode->next = l->next;
     l->next = newNode;
+}
 
+void printList(list l) {
+    node* p = l->next;
+    while (p != NULL) {
+        printf("%d -> ", p->element);
+        p = p->next;
+    }
 }
 
 int main(){
     list l = makenull();
-    
+    insertafterhead(10, l);
+    insertafterhead(12, l);
+    insertafterhead(13, l);
+    printList(l);
     return 0;
 }
